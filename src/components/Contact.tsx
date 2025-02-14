61f11b50430b500fc79e1be10d7523e95280bf4e
CallusCEO <dow.devotion@gmail.com>
2025-02-14 22:30:00 +0100

    finished the footer

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -13,15 +13,15 @@ const Contact = () => {
 		if (content) {
 			setContent('');
 			setText('Message sent!');
-			if (content.length < 5) setText('Hoot hoot! Seriously? 🦉');
+			if (content.length < 5) {
+				setText('Hoot hoot! Seriously? 🦉');
+			}
 			setVisible(true);
 			setTimeout(() => setVisible(false), 3000);
 		}
 	};
 
-	const handleInputChange = (
-		event: React.ChangeEvent<HTMLTextAreaElement>
-	) => {
+	const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
 		setContent(event.target.value);
 	};
 
@@ -39,16 +39,33 @@ const Contact = () => {
 						onChange={handleInputChange}
 					></textarea>
 					<button onClick={handleButtonClick}>Send</button>
-					<p style={{ display: visible ? 'block' : 'none' }}>
-						{text}
-					</p>
+					<p style={{ display: visible ? 'block' : 'none' }}>{text}</p>
 				</div>
 			</div>
+
 			<div id='communication'>
 				<h2>Our socials</h2>
-                <div id="icons">
-                    <div className="discord"></div>
-                </div>
+				<div id='icons'>
+					<a href='https://www.discord.com'>
+						<div className='discord'></div>
+					</a>
+					<a href='https://www.youtube.com'>
+						<div className='youtube'></div>
+					</a>
+				</div>
+			</div>
+
+			<div id="others">
+				<div className="about">
+					<h3>About us</h3>
+					<p>Our team are creating at first a beta version to let you experiment tons of things with our app for free! So don't hesitate <span>giving us some feedbacks</span> to let us improve your user experience.</p>
+				</div>
+
+				<div id="grid-nums">
+					<h6>06 95 89 78 49</h6>
+					<h6>dow.devotion@gmail.com</h6>
+					<h6><span>Made by Callus' team.</span></h6>
+				</div>
 			</div>
 		</footer>
 	);
