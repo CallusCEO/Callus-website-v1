f82b10df9fc4d06e2e983a703bbd175fe7349605
CallusCEO <dow.devotion@gmail.com>
2025-02-15 21:33:45 +0100

    ok vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/callus-website-v1"
+  base: "/callus-website-v1/",
+  build: {
+    outDir: 'dist', // The directory where the build files go
+    assetsDir: 'assets', // Directory for assets
+  },
 })
